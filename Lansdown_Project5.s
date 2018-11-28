d7e14617700a15c4cce0c2e7df3294e97aed3999
James <jlansdown@gmail.com>
2018-11-28 02:23:39 -0800

    Started add subroutine

--- Lansdown_Project5.s
+++ Lansdown_Project5.s
@@ -1,4 +1,89 @@
 .data
 
+	.align 2
+	records: .space 480 		#2 ints
+	inputMessage: .asciiz "Enter in the name, age and ID for 10 students please: "
+	name: .asciiz "Name: "
+	age: .asciiz "Age: "
+	idNumber: .asciiz "idNumber: "
+	newLine: .asciiz "\n"
+
+
 
 .text
+
+#Main 
+main:
+
+	#Displays inputMessage
+	li $v0, 4
+	la $a0, inputMessage
+	syscall
+
+
+	addi $t0, $zero, 0
+
+	#Call storeArray
+	addiu $sp, $sp, -4
+	sw $ra, 0($sp)
+	jal storeArray
+	lw $ra, 0($sp)
+	addiu $sp, $sp, 4
+
+
+
+
+	#End of program
+	li $v0, 10
+	syscall
+
+
+#STORE ARRAY - Subroutine
+storeArray:
+	
+
+	#li $v0, 4
+	#la $a0, newLine
+	#syscall
+	
+	#li $v0, 4
+	#la $a0, name
+	#syscall
+
+	#Read Name string 40 bytes
+		
+	li $v0, 8
+	syscall
+	sw $v0, records($t0)
+	addi $t0, $t0, 40
+
+	li $v0, 4
+	la $a0, age
+	syscall
+
+	#Read Age int 4 bytes
+	li $v0, 5
+	syscall
+	sw $v0, records($t0)
+	addi $t0, $t0, 4
+
+	li $v0, 4
+	la $a0, idNumber
+	syscall
+
+	#Read ID # int 4 bytes
+	li $v0, 5
+	syscall
+	sw $v0, records($t0)
+	addi $t0, $t0, 4
+
+	blt $t0, 480, storeArray
+	addi $zero, $zero, 0
+
+	jr $ra
+
+
+
+
+
+
