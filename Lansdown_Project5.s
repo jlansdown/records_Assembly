e1dab7e5da557e5b87e539160fa97bacb16752ea
James <jlansdown@gmail.com>
2018-11-28 21:34:30 -0800

    Working store and print - newLine problem

--- Lansdown_Project5.s
+++ Lansdown_Project5.s
@@ -2,6 +2,7 @@
 
 	.align 2
 	records: .space 480 		#2 ints
+	tempArr: .space 40
 	inputMessage: .asciiz "Enter in the name, age and ID for 10 students please: "
 	name: .asciiz "Name: "
 	age: .asciiz "Age: "
@@ -20,8 +21,9 @@ main:
 	la $a0, inputMessage
 	syscall
 
-	li $v0, 4
-	la $a0, newLine
+	#Newline
+	li $v0, 11
+	la $a0, 13
 	syscall
 
 
@@ -34,9 +36,11 @@ main:
 	lw $ra, 0($sp)
 	addiu $sp, $sp, 4
 
-
+	#Initialize records array to 0
 	addi $t0, $zero, 0
-	addi $t3, $zero, 1 #counter
+
+	#Conter for record number display
+	addi $t3, $zero, 1 
 
 	#Call printArr
 	addiu $sp, $sp, -4
@@ -59,16 +63,30 @@ storeArray:
 ############Read Name string 40 bytes --- Needs to be done with loop to char array
 		
 	li $v0, 8
+	la $a0, tempArr
+	li $a1, 40
 	syscall
-	sw $v0, records($t0)
-	addi $t0, $t0, 40
 
+		#initialize tempArr index to 0
+		addi $t3, $zero, 0
 
+		#Copy tempArr into records array
+		readName:
+			lb $t2, tempArr($t3)
+			sb $t2, records($t0)
+			addi $t3, $t3, 1
+			addi $t0, $t0, 1
+			beq $t2, $zero, exitReadName
+			j readName		
+
+		exitReadName:
+
+	#Restores position of $t0 for records array
+	subu $t0, $t0, $t3 
+
+	#increment to next int
+	addi $t0, $t0, 40		
 	
-
-
-
-
 
 
 #######################################
@@ -78,6 +96,8 @@ storeArray:
 	li $v0, 5
 	syscall
 	sw $v0, records($t0)
+
+	#increment to next int
 	addi $t0, $t0, 4
 
 
@@ -85,15 +105,16 @@ storeArray:
 	li $v0, 5
 	syscall
 	sw $v0, records($t0)
+
+	#increment to beginning of new struct
 	addi $t0, $t0, 4
 
 	blt $t0, 96, storeArray ###############change t0 480 for final
-	addi $zero, $zero, 0
 
 	jr $ra
 
 
-
+#PRINT ARRAY - Subroutine
 printArr:
 
 	li $v0, 4
@@ -101,7 +122,7 @@ printArr:
 	syscall
 
 	li $v0, 1
-	move $a0, $t3   #test word
+	move $a0, $t3   #record count
 	syscall
 
 	li $v0, 4
@@ -110,24 +131,30 @@ printArr:
 
 
  ##########Print Name###################
-	li $v0, 11
-	lw $a0, records($t0)
-	syscall
+ 	addi $t4, $zero, 0
+	printName:
+		
+		li $v0, 11
+		lb $a0, records($t0)
+		beq $a0, $zero, exitPrintName
+		syscall
+
+		addi $t4, $t4, 1	#Counter for char array size to restore #t0 position
+		addi $t0, $t0, 1
+		j printName
  	
+
+ 	exitPrintName:
+
+ 	#Restores $t0 position for records
+ 	subu $t0, $t0, $t4
+
  	#print space
 	li $v0, 11
 	la $a0, 32
 	syscall
 
 	addi $t0, $t0, 40
-
-
-
-
-
-
-
-
 
 
 ###########################################
@@ -139,8 +166,9 @@ printArr:
 
 	addi $t0, $t0, 4
 
+	#print space char
 	li $v0, 11
-	la $a0, 32  #print space char
+	la $a0, 32  
 	syscall
 
 
